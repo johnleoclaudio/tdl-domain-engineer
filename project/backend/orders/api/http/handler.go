c2c2b909203eb63cb300565223b3cdffee698d90
Leo Claudio <11918571+zllec@users.noreply.github.com>
2026-04-27 08:25:38 +0800

    completed 03-http/01-http-handler

--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -2,8 +2,9 @@ package http
 
 import (
 	"context"
-
 	"eats/backend/common"
+
+	"github.com/google/uuid"
 )
 
 type Handler struct{}
@@ -12,6 +13,15 @@ func NewHandler() Handler {
 	return Handler{}
 }
 
+func (handler Handler) RegisterCustomer(ctx context.Context, request RegisterCustomerRequestObject) (RegisterCustomerResponseObject, error) {
+	customerUUID := uuid.New()
+	return RegisterCustomer201JSONResponse{
+		CustomerUuid: customerUUID,
+	}, nil
+}
+
 func Register(ctx context.Context, e common.EchoRouter, handler Handler) error {
+	RegisterHandlers(e, NewStrictHandler(handler, nil))
+
 	return nil
 }
