546afbfa121d477254b5e06b1f30e4fa28743a42
Three Dots Labs <contact@threedotslabs.com>
2026-04-27 20:45:15 +0800

    completed 04-database/01-add-migrations

--- project/backend/orders/adapters/db/migrations/0001_init_orders.up.sql
+++ project/backend/orders/adapters/db/migrations/0001_init_orders.up.sql
@@ -1,1 +1,10 @@
--- todo: implement
+BEGIN;
+CREATE SCHEMA IF NOT EXISTS orders;
+CREATE TABLE orders.customers (
+  customer_uuid UUID PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  address JSON NOT NULL,
+  phone_number VARCHAR(50) NOT NULL
+  );
+COMMIT;
