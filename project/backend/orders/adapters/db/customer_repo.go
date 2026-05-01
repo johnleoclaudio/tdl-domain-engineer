c389c7b71923caffb59809c6f313d063cf70471c
Three Dots Labs <contact@threedotslabs.com>
2026-05-01 21:56:39 +0800

    completed 05-repository/01-implement-repository

--- project/backend/orders/adapters/db/customer_repo.go
+++ project/backend/orders/adapters/db/customer_repo.go
@@ -2,11 +2,13 @@ package db
 
 import (
 	"context"
+	"eats/backend/common"
+	"eats/backend/common/shared"
+	"eats/backend/orders/adapters/db/dbmodels"
+	"eats/backend/orders/api/http"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-
-	"eats/backend/common"
-	"eats/backend/orders/api/http"
 )
 
 type CustomerRepository struct {
@@ -24,6 +26,38 @@ func NewCustomerRepository(db *pgxpool.Pool) *CustomerRepository {
 }
 
 func (r *CustomerRepository) RegisterCustomer(ctx context.Context, customerUUID common.UUID, customer http.RegisterCustomer) error {
-	// TODO: implement me
+	queries := dbmodels.New(r.db)
+
+	commonAddress, err := openapiAddressToSharedAddress(customer)
+	if err != nil {
+		return fmt.Errorf("convert address failed: %w", err)
+	}
+
+	err = queries.InsertCustomer(ctx, dbmodels.InsertCustomerParams{
+		CustomerUuid: customerUUID,
+		Name:         customer.Name,
+		Email:        string(customer.Email),
+		Address:      commonAddress,
+		PhoneNumber:  customer.PhoneNumber,
+	})
+	if err != nil {
+		return fmt.Errorf("insert customer failed: %w", err)
+	}
 	return nil
+}
+
+func openapiAddressToSharedAddress(customer http.RegisterCustomer) (shared.Address, error) {
+	addr := customer.Address
+	sharedAddr, err := shared.NewAddress(
+		addr.Line1,
+		addr.Line2,
+		addr.PostalCode,
+		addr.City,
+		addr.CountryCode,
+	)
+	if err != nil {
+		return shared.Address{}, err
+	}
+
+	return sharedAddr, nil
 }
